ff3bdb0929427f34dffc72ab88f6629b6871541a
aloogladys <aloogladys@gmail.com>
2022-09-16 07:07:35 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
-#!/bin/bash
-gcc #include <stdio.h>
+#include <stdio.h>
+
+/**
+ *main - the entry point
+ *
+ * Description - printing to the standard output using printf function
+ *
+ * Return: 0 , alwats return 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+
 }
