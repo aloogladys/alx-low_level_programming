d60ad07ac05ea69c66270dc09a59a29a24a0c2f9
aloogladys <aloogladys@gmail.com>
2022-09-16 06:52:45 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - the entry point
+ *
+ * description - printing to the standard output using put function
+ *
+ * Return: 0 , always return 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual ");return: 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
-^C
