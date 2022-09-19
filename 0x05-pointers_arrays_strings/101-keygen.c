bc3c4859f81e69e76c9590b976690dc436e320a7
aloogladys <aloogladys@gmail.com>
2022-09-19 16:30:28 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: generated pssword
+ */
+
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));
+
+	while (x <= 2645)
+	{
+		c = rand() %
+			x += c;
+		putchar(c);
+
+
+	}
+	putchar (2772 - x);
+
+	return (0);
+
+}
