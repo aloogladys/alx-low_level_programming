7bb036c8e3590f81c5f308258cd6ad0935accb45
aloogladys <aloogladys@gmail.com>
2022-09-19 13:29:08 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file
+ * void prototypes (void)
+ * int prototypes (int)
+ * @n: parameter
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts_half(char *str);
+void puts2(char *str);
+void print_array(int *a, int b);
+char *_strcpy(char *dest, char src);
+int_atoi(char *str);
+
+#endif /*MAIN_H*/
