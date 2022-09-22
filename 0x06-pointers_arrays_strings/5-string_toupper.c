7a30746de4fd2483f3ecef9cbb007b81e0ef1289
aloogladys <aloogladys@gmail.com>
2022-09-22 13:17:28 -0700

    done number 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+char *string_toupper(char *str)
+
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+}
+	return (str);
+}
