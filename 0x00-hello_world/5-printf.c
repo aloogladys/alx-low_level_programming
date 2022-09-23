21ad9bb9062bd26e14ba15f3f03e512a2a9e82dd
aloogladys <aloogladys@gmail.com>
2022-09-22 23:54:10 -0700

    done number 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - the entry point
+ * main - Entry pointA
  *
- * Description - printing to the standard output using printf function
- *
- * Return: 0 , alwats return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-		return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
