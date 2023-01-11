c6f4181422c264c892f645a74e8d6aa38ddfb8c4
aloogladys <aloogladys@gmail.com>
2023-01-11 04:34:50 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,10 @@
 
 /**
  * create_array - Creates an array of chars and
-
  *                initializes it with a specific char.
-
  * @size: The size of the array to be initialized.
-
  * @c: The specific char to intialize the array with.
-
- *
-
  * Return: If size == 0 or the function fails - NULL.
-
  *         Otherwise - a pointer to the array.
 */
 
